baae9118469bff9b91e985caf77c94995eba04c8
Harry Chong <hchong.16@gmail.com>
2020-04-17 00:26:51 -0400

    Extraction package with test coverage

--- extraction/extraction.go
+++ extraction/extraction.go
@@ -1,5 +1,45 @@
 package extraction
 
-func Show() string{
-    return "Hello, world."
+import (
+	"os/exec"
+	"strconv"
+)
+
+// Extractor is an abstracted interface for generating the i-frames. It contains a single method,
+// GenerateFrames, which accepts the interval of i-frames to take, the path to the video file, and
+// the output directory.
+type Extractor interface {
+	GenerateFrames(n int, video string, dir string) error
+}
+
+// ExtractionService hides the Extractor interface.
+type ExtractionService struct {
+	extractor Extractor
+}
+
+// NewExtractionService creates a new ExtractionService structure.
+func NewExtractionService(e Extractor) ExtractionService {
+	return ExtractionService{
+		extractor: e,
+	}
+}
+
+// ExtractionService exports the GenerateFrames function.
+func (ex ExtractionService) IFrames(n int, video, dir string) error {
+	return ex.extractor.GenerateFrames(n, video, dir)
+}
+
+// FFMPEGExtractor is an exported structure that holds the path to the executable for FFMPEG.
+type FFMPEGExtractor struct {
+	Path string
+}
+
+// GenerateFrames is an unexported processing function attached to FMMPEGExtractor. It will
+// utilize FFMPEG in order to extract a frame for every n frames in the video and store the
+// ouputs to the dir path.
+func (f FFMPEGExtractor) GenerateFrames(n int, video string, dir string) error {
+	cmd := exec.Command("ffmpeg", "-i", video, "-vf", "select=not(mod(n\\,"+strconv.Itoa(n)+"))",
+		"-vsync", "vfr", dir+"frame%03d.png")
+
+	return cmd.Run()
 }
--- extraction/extraction_test.go
+++ extraction/extraction_test.go
@@ -1,9 +1,98 @@
 package extraction
-import "testing"
 
-func TestShow(t *testing.T) {
-    want := "Hello, world."
-	if got := Show(); got != want {
-	    t.Errorf("Hello() = %q, want %q", got, want)
-    }
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewExtractionService(t *testing.T) {
+	testCases := []struct {
+		name string
+		f    FFMPEGExtractor
+		want ExtractionService
+	}{
+		{
+			name: "successful-service-creation",
+			f: FFMPEGExtractor{
+				Path: "ffmpeg",
+			},
+			want: ExtractionService{
+				extractor: FFMPEGExtractor{
+					Path: "ffmpeg",
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			es := NewExtractionService(tc.f)
+
+			if !reflect.DeepEqual(tc.want, es) {
+				t.Errorf("got '%v', want '%v", es, tc.want)
+			}
+		})
+	}
+}
+
+func TestIFrames(t *testing.T) {
+	testCases := []struct {
+		name  string
+		f     FFMPEGExtractor
+		n     int
+		video string
+		dir   string
+	}{
+		{
+			name: "successful-iframes",
+			f: FFMPEGExtractor{
+				Path: "ffmpeg",
+			},
+			n:     10,
+			video: "../data/yosemiteA.mp4",
+			dir:   "../data/iframes/",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			es := NewExtractionService(tc.f)
+
+			err := es.IFrames(tc.n, tc.video, tc.dir)
+
+			if err != nil {
+				t.Fatalf("error in iFrames: err = %v", err)
+			}
+		})
+	}
+}
+
+func TestGenerateFrames(t *testing.T) {
+	testCases := []struct {
+		name  string
+		f     FFMPEGExtractor
+		n     int
+		video string
+		dir   string
+	}{
+		{
+			name: "successful-generate-frames",
+			f: FFMPEGExtractor{
+				Path: "ffmpeg",
+			},
+			n:     10,
+			video: "../data/yosemiteA.mp4",
+			dir:   "../data/iframes/",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.f.GenerateFrames(tc.n, tc.video, tc.dir)
+
+			if err != nil {
+				t.Fatalf("error in Generateframes: err = %v", err)
+			}
+		})
+	}
 }
